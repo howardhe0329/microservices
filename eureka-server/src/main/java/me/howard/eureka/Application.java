8482e45172071ebe2d911b4d1ad2c1b7fc3334a5
贺斌hb <hebin@jajaying.com>
2017-09-11 10:20:02 +0800

    Discovery

---
+++ eureka-server/src/main/java/me/howard/eureka/Application.java
@@ -0,0 +1,17 @@
+package me.howard.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by howard on 2017/6/23.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
